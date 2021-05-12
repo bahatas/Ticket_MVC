3ce0b87523bc9a7b0bd25bc4e6f21089e42884c9
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-05-12 13:07:04 +0100

    Initial commit

---
+++ src/main/java/com/ticket_mvc/TicketMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ticket_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticket_mvc/TicketMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
