c0474bfd5cfc15acaf696fe4d02a39587a62cf5f
bahatas <baha@cybetekschool.com>
2021-05-13 22:17:29 +0100

    User Create DTO/Controller/ Html

---
+++ src/main/java/com/ticket_mvc/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.ticket_mvc.controller;
+
+
+import com.ticket_mvc.dto.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/create")
+    public String userCreate(Model model){
+
+        model.addAttribute("user", new UserDTO());
+
+
+
+        return "create";
+
+
+
+
+
+    }
+}
