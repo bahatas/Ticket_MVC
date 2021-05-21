e51685504b927a361b17b5636b886fa479d3a7f9
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-05-21 13:18:31 +0100

    MVC project

--- src/main/java/com/ticket_mvc/controller/TaskController.java
+++ src/main/java/com/ticket_mvc/controller/TaskController.java
@@ -4,11 +4,16 @@ import com.ticket_mvc.dto.TaskDTO;
 import com.ticket_mvc.service.ProjectService;
 import com.ticket_mvc.service.TaskService;
 import com.ticket_mvc.service.UserService;
+import com.ticket_mvc.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 
 @Controller
@@ -33,6 +38,18 @@ public class TaskController {
 
 
         return "/task/create";
+    }
+
+    @PostMapping("/create")
+    public String task(Model model, TaskDTO taskDTO) {
+
+        taskDTO.setTaskStatus(Status.OPEN);
+        taskDTO.setAssignedDate(LocalDate.now());
+        taskDTO.setId(UUID.randomUUID().getMostSignificantBits());
+        taskService.save(taskDTO);
+
+
+        return "redirect:/task/create";
     }
 
 
